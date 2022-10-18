b9a250fc817b0612d533af94bde58d2c2393341e
Jeffrey Margulieux <jeff.margulieux@buhlschools.org>
2022-10-17 20:50:52 -0600

    update3

---
+++ build/helloworld.js
@@ -0,0 +1,7 @@
+"use strict";
+var message = "Hello World";
+console.log(message);
+function test() {
+    return (message);
+}
+//# sourceMappingURL=helloworld.js.map
---
+++ ts/helloworld.ts
@@ -0,0 +1,6 @@
+let message: string = "Hello World";
+console.log(message);
+
+function test(){
+    return(message);
+}
