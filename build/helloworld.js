37f6a1aa18cb76ebee3dd5813904e08e4a822902
Jeffrey Margulieux <jeff.margulieux@buhlschools.org>
2022-10-18 20:28:38 -0600

    update4

--- build/helloworld.js
+++ build/helloworld.js
@@ -4,4 +4,17 @@ console.log(message);
 function test() {
     return (message);
 }
+//I cannot figure out how to make the listener only apply to the button and not the whole page
+document. /*getElementById("code").*/addEventListener("click", swapCode);
+function swapCode() {
+    var code1 = "<strong>JavaScript</strong><br><br>let variable = \"Hello World\";<br><br>";
+    var code2 = "<strong>TypeScript</strong><br><br>let variable: string = \"Hello World\";<br><br>";
+    var code = document.getElementById("code").innerHTML;
+    if (code1 === code) {
+        document.getElementById("code").innerHTML = code2;
+    }
+    else {
+        document.getElementById("code").innerHTML = code1;
+    }
+}
 //# sourceMappingURL=helloworld.js.map
--- ts/helloworld.ts
+++ ts/helloworld.ts
@@ -1,6 +1,21 @@
-let message: string = "Hello World";
+let message = "Hello World";
 console.log(message);
 
-function test(){
-    return(message);
+function test() {
+    return (message);
+}
+
+//I cannot figure out how to make the listener only apply to the button and not the whole page
+document./*getElementById("code").*/addEventListener("click", swapCode);
+
+function swapCode(){
+    let code1 = "<strong>JavaScript</strong><br><br>let variable = \"Hello World\";<br><br>";
+    let code2 = "<strong>TypeScript</strong><br><br>let variable: string = \"Hello World\";<br><br>";
+    let code = document.getElementById("code").innerHTML;
+    if (code1 === code){
+        document.getElementById("code").innerHTML = code2;
+    }
+    else{
+        document.getElementById("code").innerHTML = code1;
+    }
 }
